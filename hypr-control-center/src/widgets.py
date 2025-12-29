7c9eaa778a89ec46bfbc1efbff563c6604ee06c0
gekinzen <yukipaul8@gmail.com>
2025-12-29 16:44:52 +0800

    new added theme dynamic

--- hypr-control-center/src/widgets.py
+++ hypr-control-center/src/widgets.py
@@ -4,7 +4,8 @@ Custom GTK widgets for Hyprland Control Center
 
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
 from typing import Callable
 
 from .utils import rgba_to_gdk, gdk_to_rgba
